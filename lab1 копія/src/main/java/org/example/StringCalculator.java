a7a13ccd81396c66a5d0371cf23faa0393828038
brovarskyiartem <partysite47@gmail.com>
2023-09-22 12:11:40 +0300

    step1

---
+++ lab1 копія/src/main/java/org/example/StringCalculator.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class StringCalculator {
+
+    public int add(String numbers) {
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+
+        String[] numberArray = numbers.split(",");
+        int sum = 0;
+
+        for (String number : numberArray) {
+            try {
+                int num = Integer.parseInt(number);
+                sum += num;
+            } catch (NumberFormatException e) {
+                // Обробляємо помилку, якщо введено нечисловий рядок
+                System.err.println("Помилка: " + e.getMessage());
+            }
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        StringCalculator calculator = new StringCalculator();
+        String input = "1,2";
+        int result = calculator.add(input);
+        System.out.println("Сума чисел: " + result);
+    }
+}
