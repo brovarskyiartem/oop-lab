634707ece6f1a2dba258cb9b979f56f8df4e75d4
brovarskyiartem <partysite47@gmail.com>
2023-09-23 14:35:26 +0300

    lab1-step5

--- lab1/src/main/java/org/example/StringCalculator.java
+++ lab1/src/main/java/org/example/StringCalculator.java
@@ -1,31 +1,47 @@
 package org.example;
-
+import java.util.ArrayList;
+import java.util.List;
 public class StringCalculator {
-
     public int add(String numbers) {
         if (numbers.isEmpty()) {
             return 0;
         }
-
-        String[] numberArray = numbers.split(",");
+        String[] numberArray;
+        if (numbers.startsWith("//")) {
+            int end_index = numbers.length();
+            char delimiter = numbers.charAt(2);
+            numberArray = numbers.substring(4, end_index).split(String.valueOf("\\" + delimiter));
+        } else {
+            numberArray = numbers.split("[,\\n]");
+        }
         int sum = 0;
 
-        for (String number : numberArray) {
+        List<Integer> negativenumberArray = new ArrayList<>();
+        for (String number_neg : numberArray) {
             try {
-                int num = Integer.parseInt(number);
-                sum += num;
+                int num = Integer.parseInt(number_neg);
+                if (num < 0) {
+                    negativenumberArray.add(num);
+                } else {
+                    sum += num;
+                }
             } catch (NumberFormatException e) {
-                System.err.println("Помилка: " + e.getMessage());
+                throw new RuntimeException("Not correct input");
             }
         }
-
+        if (!negativenumberArray.isEmpty()) {
+            throw new RuntimeException("Negative numbers are not allowed: " + negativenumberArray);
+        }
         return sum;
     }
-
     public static void main(String[] args) {
         StringCalculator calculator = new StringCalculator();
-        String input = "";
-        int result = calculator.add(input);
-        System.out.println("Сума: " + result);
+        String input = "//.\n1.2";
+        try {
+            int result = calculator.add(input);
+            System.out.println("Сума: " + result);
+        }catch (RuntimeException e){
+            System.err.println(e.getMessage());
+            }
+        }
     }
-}
