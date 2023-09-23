342a37b0eca0418ea0f6384f8e4d59a2ca031f33
brovarskyiartem <145754242+brovarskyiartem@users.noreply.github.com>
2023-09-23 14:29:04 +0300

    Update StringCalculator.java

--- lab1/src/main/java/org/example/StringCalculator.java
+++ lab1/src/main/java/org/example/StringCalculator.java
@@ -1,30 +1,38 @@
 package org.example;
-
 public class StringCalculator {
 
     public int add(String numbers) {
         if (numbers.isEmpty()) {
             return 0;
         }
-
-        String[] numberArray = numbers.split(",");
+        String if_delimiter = "";
+        String[] numberArray;
+        if (numbers.startsWith("//")) {
+            int end_index = numbers.length();
+            char delimiter = numbers.charAt(2);
+            numberArray = numbers.substring(4, end_index).split(String.valueOf("\\" + delimiter));
+        } else {
+            numberArray = numbers.split("[,\\n]");
+        }
         int sum = 0;
+
 
         for (String number : numberArray) {
             try {
                 int num = Integer.parseInt(number);
                 sum += num;
             } catch (NumberFormatException e) {
-                System.err.println("Помилка: " + e.getMessage());
+                return 0;
             }
         }
 
         return sum;
     }
 
+
     public static void main(String[] args) {
         StringCalculator calculator = new StringCalculator();
-        String input = "";
+        String input = "//,\n1,2,3";
         int result = calculator.add(input);
         System.out.println("Сума: " + result);
     }
