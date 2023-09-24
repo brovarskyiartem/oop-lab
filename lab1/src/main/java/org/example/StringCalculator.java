369093d77916d0789b79fc65aa0241331db6b3bb
brovarskyiartem <partysite47@gmail.com>
2023-09-24 13:28:07 +0300

    lab1-step7

--- lab1/src/main/java/org/example/StringCalculator.java
+++ lab1/src/main/java/org/example/StringCalculator.java
@@ -1,31 +1,59 @@
 package org.example;
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 public class StringCalculator {
-
     public int add(String numbers) {
         if (numbers.isEmpty()) {
             return 0;
         }
-
-        String[] numberArray = numbers.split(",");
+        String[] numberArray;
+        if (numbers.startsWith("//")) {
+            String delim = String.valueOf(numbers.charAt(2));
+            if(!delim.equals("[")){
+                int end_index = numbers.length();
+                char delimiter = numbers.charAt(2);
+                numberArray = numbers.substring(4, end_index).split(String.valueOf("\\" + delimiter));
+            }else{
+                char delimiter = numbers.charAt(3);
+                numbers = numbers.replaceAll("(.)\\1+", "$1");
+                int end_index = numbers.length();
+                numberArray = numbers.substring(5, end_index).split(String.valueOf("\\" + delimiter ));
+            }
+        } else {
+            numberArray = numbers.split("[,\\n]");
+        }
         int sum = 0;
 
-        for (String number : numberArray) {
+        List<Integer> negativenumberArray = new ArrayList<>();
+        for (String number_neg : numberArray) {
             try {
-                int num = Integer.parseInt(number);
-                sum += num;
+                int num = Integer.parseInt(number_neg);
+                if (num < 0) {
+                    negativenumberArray.add(num);
+                } else if(num > 1000) {
+                 continue;
+                }
+                else {
+                    sum += num;
+                }
             } catch (NumberFormatException e) {
-                System.err.println("Помилка: " + e.getMessage());
+                throw new RuntimeException("Not correct input");
             }
         }
-
+        if (!negativenumberArray.isEmpty()) {
+            throw new RuntimeException("Negative numbers are not allowed: " + negativenumberArray);
+        }
         return sum;
     }
-
     public static void main(String[] args) {
         StringCalculator calculator = new StringCalculator();
-        String input = "";
-        int result = calculator.add(input);
-        System.out.println("Сума: " + result);
+        String input = "//[{]\n1{{2";
+        try {
+            int result = calculator.add(input);
+            System.out.println("Сума: " + result);
+        }catch (RuntimeException e){
+            System.err.println(e.getMessage());
+            }
+        }
     }
-}
