aa7038e04dc463ee3d70d0e27a79cec86cb55479
brovarskyiartem <145754242+brovarskyiartem@users.noreply.github.com>
2023-10-01 17:02:16 +0300

    Update StringCalculator.java

--- lab1/src/main/java/org/example/StringCalculator.java
+++ lab1/src/main/java/org/example/StringCalculator.java
@@ -2,7 +2,6 @@ package org.example;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
@@ -19,13 +18,15 @@ public class StringCalculator {
                 numberArray = numbers.substring(4, end_index).split(String.valueOf("\\" + delimiter));
             }else{
                 numbers = numbers.replaceAll("(.)\\1+", "$1");
+                numbers = numbers.replaceAll("[\n]+", "\n");
                 String customDelim = numbers.substring(2, numbers.indexOf("\n"));
-                String newNumbers = numbers.substring(numbers.indexOf("\n") + 1, numbers.length());
+                String newNumbers = numbers.substring(numbers.indexOf("\n") + 1);
                 customDelim = customDelim.replace("[", "");
                 String[] delimArray = customDelim.split("]");
                 for (String delimiter : delimArray) {
                     newNumbers = newNumbers.replaceAll(Pattern.quote(delimiter), ",");
                 }
+                newNumbers = newNumbers.replace("\n",",");
                 numberArray = newNumbers.split(",");
             }
         } else {
@@ -39,7 +40,7 @@ public class StringCalculator {
                 if (num < 0) {
                     negativenumberArray.add(num);
                 } else if(num > 1000) {
-                 continue;
+                    continue;
                 }
                 else {
                     sum += num;
@@ -55,12 +56,12 @@ public class StringCalculator {
     }
     public static void main(String[] args) {
         StringCalculator calculator = new StringCalculator();
-        String input = "//[{][*]\n1{2*3";
+        String input = "//[{][*]\n1{2,,,,3\n\n\n3*2";
         try {
             int result = calculator.add(input);
             System.out.println("Сума: " + result);
         }catch (RuntimeException e){
             System.err.println(e.getMessage());
-            }
         }
     }
+}
